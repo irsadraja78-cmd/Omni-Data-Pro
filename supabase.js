152e399b36e17384666119d4c1ab4fe14b7f0d50
irsadraja78-cmd <irsadraja78@gmail.com>
2026-07-25 17:27:08 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,29 +1,11 @@
-// =====================================
-// OmniDataPro Supabase Connection
-// =====================================
-
-
-// अपना Supabase Project URL यहाँ डालें
+// Supabase.js
 
 const SUPABASE_URL = "https://qvmywidjilwwkqvstglo.supabase.co";
-
-
-// अपनी Supabase anon public key यहाँ डालें
-
 const SUPABASE_ANON_KEY = "sb_publishable_4HhTJOl3003RLkW_DQR6-w_Kg01Vh5U";
-
-
-
-
-// Supabase Client Create
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-
-
-// Export
-
-window.supabaseClient = supabaseClient;
+export default supabaseClient;
