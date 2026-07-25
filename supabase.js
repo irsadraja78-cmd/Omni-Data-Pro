52fd2c33eed971ac42504b6da33c5902b1fdd163
irsadraja78-cmd <irsadraja78@gmail.com>
2026-07-25 21:25:27 +0530

    Fix export statement for supabaseClient

--- supabase.js
+++ supabase.js
@@ -1,11 +1,28 @@
+// ===================================
+// OmniData Pro
 // Supabase.js
+// ===================================
+
+
+// Supabase Configuration
 
 const SUPABASE_URL = "https://qvmywidjilwwkqvstglo.supabase.co";
+
 const SUPABASE_ANON_KEY = "sb_publishable_4HhTJOl3003RLkW_DQR6-w_Kg01Vh5U";
+
+
+
+
+// Create Supabase Client
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+
+
+
+// Export Client
 
 export default supabaseClient;
