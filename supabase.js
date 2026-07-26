06060067719dbd9199f51f4db13aab57745fbf87
irsadraja78-cmd <irsadraja78@gmail.com>
2026-07-26 13:16:37 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,8 +4,8 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://qvmywidjilwwkqvstglo.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_4HhTJOl3003RLkW_DQR6-w_Kg01Vh5U';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
